9588f326d5f6d31f9a576fbe870d5ea43fa64f43
AcePeaX <benmansour.mohammed2002@gmail.com>
2024-11-14 00:00:12 +0100

    Other tries

--- lib/utils.py
+++ lib/utils.py
@@ -11,7 +11,7 @@ def preprocess(image, device=None):
     transform = transforms.Compose([
         transforms.Resize((512, 512)),
         transforms.ToTensor(),
-        normalize
+        #normalize
     ])
     return transform(image).unsqueeze(0).to(device)  # Add batch dimension
 
@@ -21,6 +21,6 @@ def deprocess(tensor):
         mean=[-0.485/0.229, -0.456/0.224, -0.406/0.225],
         std =[1/0.229, 1/0.224, 1/0.225]
     )
-    tensor = inv_normalize(tensor.squeeze(0).cpu())
+    tensor = tensor.squeeze(0).cpu() #inv_normalize(tensor.squeeze(0).cpu())
     tensor = torch.clamp(tensor, 0, 1)
     return transforms.ToPILImage()(tensor)
