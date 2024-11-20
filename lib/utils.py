4f8cb0ecd43c34385fd71c3a8595ceca74efe097
AcePeaX <benmansour.mohammed2002@gmail.com>
2024-11-20 13:48:04 +0100

    Added gram matrix

--- lib/utils.py
+++ lib/utils.py
@@ -4,15 +4,20 @@ from torchvision import transforms
 # Define normalization transform
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                 std =[0.229, 0.224, 0.225])
-def preprocess(image, device=None):
+def preprocess(image, device=None, resize=True):
     if device==None:
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Assume 'image' is a PIL Image
-    transform = transforms.Compose([
-        transforms.Resize((512, 512)),
-        transforms.ToTensor(),
-        #normalize
-    ])
+    if resize:
+        transform = transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor(),
+        ])
+    else:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+
     return transform(image).to(device)  # Add batch dimension
 
 def deprocess(tensor):
@@ -26,3 +31,32 @@ def deprocess(tensor):
     tensor = tensor.cpu() #inv_normalize(tensor.squeeze(0).cpu())
     tensor = torch.clamp(tensor, 0, 1)
     return transforms.ToPILImage()(tensor)
+
+def gram_matrix(X: torch.Tensor):
+    """
+    Compute the Gram matrix for a batch of feature maps.
+
+    Args:
+        X (torch.Tensor): Input tensor of shape (b, c, h, w), where
+                                b = batch size,
+                                c = number of channels,
+                                h = height,
+                                w = width.
+
+    Returns:
+        torch.Tensor: Gram matrix for each image in the batch, of shape (b, c, c).
+    """
+
+    if len(X.shape)==4:
+        (b, ch, h, w) = X.shape
+        features = X.view(b, ch, w * h)
+        features_t = features.transpose(1, 2)
+        gram = features.bmm(features_t) / (ch * h * w)
+    else:
+        (ch, h, w) = X.shape
+        features = X.view(ch, w * h)
+        features_t = features.transpose(0, 1)
+        gram = features.matmul(features_t) / (ch * h * w)
+    return gram
+
+
